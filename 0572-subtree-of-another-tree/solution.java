842565104e493fd531636cd268a3c6461620a8e7
Addison Kauzer <addisonjkauzer@gmail.com>
2023-08-11 00:54:03 -0700

    solve: subtree-of-another-tree (java)

---
+++ 0572-subtree-of-another-tree/solution.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+        if (isSubtree(root.left, subRoot)) {
+            return true;
+        }
+        if (isSubtree(root.right, subRoot)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
+        }
+        if (p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
