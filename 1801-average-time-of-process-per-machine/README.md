b371c221f1cffaff732edf366a555bc007bf1ba6
Addison Kauzer <addisonjkauzer@gmail.com>
2026-01-02 16:12:35 -0800

    solve: average-time-of-process-per-machine (mysql)

---
+++ 1801-average-time-of-process-per-machine/README.md
@@ -0,0 +1,3 @@
+# 1801. Average Time of Process per Machine
+
+https://leetcode.com/problems/average-time-of-process-per-machine/
---
+++ 1801-average-time-of-process-per-machine/solution.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT start.machine_id, ROUND(AVG(end.timestamp - start.timestamp), 3) as processing_time FROM Activity AS start JOIN Activity AS end ON start.machine_id = end.machine_id AND start.process_id = end.process_id AND start.activity_type = 'start' AND end.activity_type = 'END' GROUP BY machine_id;
